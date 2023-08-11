ff7589ab79882cca7a53ca738174f5fa6ea261c2
Janice <owusujanice799@gmail.com>
2023-08-11 18:17:23 +0000

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabeit
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+		putchar(n);
+	for (n = 'A'; n <= 'Z'; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
+
